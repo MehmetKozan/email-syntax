8dd1c78d403d2521f50eefc8ba440417581aef04
Arthur Groupp <arth.groupp@gmail.com>
2017-08-07 17:02:59 +0300

    Full refactoring of library to class Remake of test suite to satisfy class style testing

--- README.md
+++ README.md
@@ -4,3 +4,29 @@ Email addresses syntax validations library
 [![Build Status](https://travis-ci.org/agroupp/email-syntax.svg?branch=master)](https://travis-ci.org/agroupp/email-syntax)
 [![Coverage Status](https://coveralls.io/repos/github/agroupp/email-syntax/badge.svg?branch=master)](https://coveralls.io/github/agroupp/email-syntax?branch=master)
 
+Syntax email addresses verification based on [RFC5321](https://tools.ietf.org/html/rfc5321) and 
+[RFC5322](https://tools.ietf.org/html/rfc5322).
+
+
+## Installation
+Install Email Syntax as an npm module and save it to your package.json file as a dependency:
+    
+    npm install --save email-syntax
+
+
+## Usage
+To use validator, you need to create instance of EmailSyntax class and use its main method validate(). Furthermore, 
+you can use method extractFromQuotes() to get local part of address without quotes.
+
+```javascript
+    const emailSyntax = new EmailSyntax();
+    if (emailSyntax.validate('test@some-mail.com')){
+        console.log('This address is valid');
+    }
+```
+
+
+## Tests
+
+    npm test
+
--- test/syntax-validation.test.js
+++ test/syntax-validation.test.js
@@ -1,70 +1,114 @@
 'use strict';
 
 const assert = require('chai').assert;
-const syntaxValidate = require('../');
+const EmailSyntax = require('../');
 
 describe('Syntax validation of email address', function(){
+    let syntaxValidate;
     describe('Address splitting', function(){
-        it('splits email address to local part and domain name', function(){
-            assert.deepEqual(syntaxValidate.split('email@domain.com'), { localPart: 'email', domainName: 'domain.com'});
+        beforeEach(function(){
+            syntaxValidate = new EmailSyntax();
         });
-        it('returns empty object if email address is not valid for splitting', function(){
-            assert.deepEqual(syntaxValidate.split('email@domain@com'), {});
-            assert.deepEqual(syntaxValidate.split('email'), {});
-            assert.deepEqual(syntaxValidate.split(''), {});
-            assert.deepEqual(syntaxValidate.split(), {});
+
+        it('splits email address to local part and domain name', function(){
+            syntaxValidate._split('test1@domain.com');
+            assert.strictEqual(syntaxValidate.localPart, 'test1');
+            assert.strictEqual(syntaxValidate.domainName, 'domain.com');
+        });
+        it("doesn't set properties if email address is not valid for splitting", function(){
+            syntaxValidate._split('email@domain@com');
+            assert.isUndefined(syntaxValidate.localPart);
+            assert.isUndefined(syntaxValidate.domainName);
+
+            syntaxValidate._split('email');
+            assert.isUndefined(syntaxValidate.localPart);
+            assert.isUndefined(syntaxValidate.domainName);
+
+            syntaxValidate._split('');
+            assert.isUndefined(syntaxValidate.localPart);
+            assert.isUndefined(syntaxValidate.domainName);
+
+            syntaxValidate._split();
+            assert.isUndefined(syntaxValidate.localPart);
+            assert.isUndefined(syntaxValidate.domainName);
         });
     });
     describe('Local part', function(){
         it("may be quoted", function(){
             assert.equal(syntaxValidate.extractFromQuotes('"abc"'), 'abc');
+            assert.equal(syntaxValidate.extractFromQuotes('abc'), 'abc');
             assert.lengthOf(syntaxValidate.extractFromQuotes('"ab"c'), 0);
             assert.lengthOf(syntaxValidate.extractFromQuotes('"ab"cdlkf"'), 0);
         });
         it("may use uppercase and lowercase Latin letters A to Z and a to z", function(){
-            assert(syntaxValidate.validateLocalPartSyntax('abc'));
-            assert(syntaxValidate.validateLocalPartSyntax('aBcxYz'));
+            syntaxValidate.localPart = 'abc';
+            assert.isTrue(syntaxValidate._validateLocalPartSyntax());
+            syntaxValidate.localPart = 'aBcxYz';
+            assert.isTrue(syntaxValidate._validateLocalPartSyntax());
         });
         it("may use digits 0 to 9", function(){
-            assert(syntaxValidate.validateLocalPartSyntax('abc3748374'));
-            assert(syntaxValidate.validateLocalPartSyntax('9894385984abc'));
-            assert(syntaxValidate.validateLocalPartSyntax('9859485984'));
+            syntaxValidate.localPart = 'abc3748374';
+            assert.isTrue(syntaxValidate._validateLocalPartSyntax());
+            syntaxValidate.localPart = '9894385984abc';
+            assert.isTrue(syntaxValidate._validateLocalPartSyntax());
+            syntaxValidate.localPart = '9859485984';
+            assert.isTrue(syntaxValidate._validateLocalPartSyntax());
         });
         it("may use special characters !#$%&'*+-/=?^_`{|}~", function(){
-             assert(syntaxValidate.validateLocalPartSyntax('!#$%&\'*+-/=?^_`{|}~'));
+            syntaxValidate.localPart = '!#$%&\'*+-/=?^_`{|}~';
+             assert.isTrue(syntaxValidate._validateLocalPartSyntax());
         });
         it("dot '.' may be used but not first, last or appear consecutively", function(){
-            assert(syntaxValidate.validateLocalPartSyntax('ab.c'));
-            assert.isNotOk(syntaxValidate.validateLocalPartSyntax('.abc'));
-            assert.isNotOk(syntaxValidate.validateLocalPartSyntax('abc.'));
-            assert.isNotOk(syntaxValidate.validateLocalPartSyntax('ab..c'));
+            syntaxValidate.localPart = 'ab.c';
+            assert.isTrue(syntaxValidate._validateLocalPartSyntax());
+            syntaxValidate.localPart = '.abc';
+            assert.isFalse(syntaxValidate._validateLocalPartSyntax());
+            syntaxValidate.localPart = 'abc.';
+            assert.isFalse(syntaxValidate._validateLocalPartSyntax());
+            syntaxValidate.localPart = 'ab..c';
+            assert.isFalse(syntaxValidate._validateLocalPartSyntax());
         });
     });
     describe('Domain name', function(){
         it("can be valid IP address", function(){
-            assert(syntaxValidate.isValidIpAddress('172.68.14.55'));
-            assert.isNotOk(syntaxValidate.isValidIpAddress('172.68.314.55'));
-            assert.isNotOk(syntaxValidate.isValidIpAddress('172.68.14.55.77'));
-            assert.isNotOk(syntaxValidate.isValidIpAddress('172.14.55'));
+            syntaxValidate.domainName = '172.68.14.55';
+            assert.isTrue(syntaxValidate._isValidIpAddress());
+            syntaxValidate.domainName = '172.68.314.55';
+            assert.isNotTrue(syntaxValidate._isValidIpAddress());
+            syntaxValidate.domainName = '172.68.14.55.77';
+            assert.isNotTrue(syntaxValidate._isValidIpAddress());
+            syntaxValidate.domainName = '172.14.55';
+            assert.isNotTrue(syntaxValidate._isValidIpAddress());
         });
         it("every part may use only ASCII letters, digits and '-' case-insensitive", function(){
-            assert(syntaxValidate.validateDomainName('abc.xyz'));
-            assert.isNotOk(syntaxValidate.validateDomainName('abc.xyz.'));
-            assert(syntaxValidate.validateDomainName('ab-c.xyz'));
+            syntaxValidate.domainName = 'abc.xyz';
+            assert.isTrue(syntaxValidate._validateDomainName());
+            syntaxValidate.domainName = 'abc.xyz.';
+            assert.isNotTrue(syntaxValidate._validateDomainName());
+            syntaxValidate.domainName = 'ab-c.xyz';
+            assert.isTrue(syntaxValidate._validateDomainName());
         });
         it("every part may start only with letters or digits", function(){
-            assert.isNotOk(syntaxValidate.validateDomainName('!kdlfd.-lkds'));
-            assert(syntaxValidate.validateDomainName('123.abc'));
+            syntaxValidate.domainName = '!kdlfd.-lkds';
+            assert.isFalse(syntaxValidate._validateDomainName());
+            syntaxValidate.domainName = '123.abc';
+            assert.isTrue(syntaxValidate._validateDomainName());
         });
         it("every part can't end with hyphen", function(){
-            assert.isNotOk(syntaxValidate.validateDomainName('abc.xyz-'));
+            syntaxValidate.domainName = 'abc.xyz-';
+            assert.isFalse(syntaxValidate._validateDomainName());
         });
         it("last part must be more then 1 symbol", function(){
-            assert.isNotOk(syntaxValidate.validateDomainName('abc.x'));
+            syntaxValidate.domainName = 'abc.x';
+            assert.isFalse(syntaxValidate._validateDomainName());
         });
     });
 });
 describe('Common address validations', function(){
+    let syntaxValidate;
+    beforeEach(function(){
+        syntaxValidate = new EmailSyntax();
+    });
     it('must be in form "local-part@domain-name"', function(){
         assert(syntaxValidate.validate('abc@domain'));
         assert.isNotOk(syntaxValidate.validate('abc@domain@domain2'));
@@ -85,6 +129,9 @@ describe('Common address validations', function(){
         assert.isNotOk(syntaxValidate.validate('email@-domain.com'));
     });
     it("can't be 'email@domain..com'", function(){
+        assert.isNotOk(syntaxValidate.validate('email@domain..com'));
+    });
+    it('can\'t be \'"em"ail"@domain.com\'', function(){
         assert.isNotOk(syntaxValidate.validate('email@domain..com'));
     });
     it("can't be 'email@'", function(){
